f3f9900cdf87cdc1cc8bfc836cdfa03b67dba33b
Umang Kumar <kumarumang4386@gmail.com>
2023-01-21 01:07:48 +0530

    Added Requirements file

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
 from django.template.defaultfilters import slugify
 from django.utils.timezone import now
 
